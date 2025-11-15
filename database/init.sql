96ed05b83d34ded0e35fdabefe4d8ae7ea822785
PetrStastny1 <Petr_Stastny@seznam.cz>
2025-11-15 14:15:46 +0100

    základ backendu s GraphQL, Fastify

---
+++ database/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS sales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    product VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
