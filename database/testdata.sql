e1f5c93a07c0052fa82541d9fde70a060054ce74
PetrStastny1 <Petr_Stastny@seznam.cz>
2025-11-17 20:44:20 +0100

    úprava testdata.sql

---
+++ database/testdata.sql
@@ -0,0 +1,22 @@
+INSERT INTO sales (date, product, quantity, price) VALUES
+('2025-01-01', 'Červené víno Merlot', 9, 249),
+('2025-01-03', 'Červené víno Merlot', 7, 259),
+('2025-01-05', 'Červené víno Merlot', 6, 269),
+('2025-01-06', 'Červené víno Merlot', 4, 289),
+('2025-01-07', 'Červené víno Merlot', 10, 229),
+('2025-01-08', 'Červené víno Merlot', 12, 199),
+('2025-01-09', 'Červené víno Merlot', 8, 249),
+('2025-01-11', 'Červené víno Merlot', 11, 219),
+
+('2025-01-02', 'Bílé víno Chardonnay', 4, 189),
+('2025-01-04', 'Bílé víno Chardonnay', 5, 189),
+('2025-01-10', 'Bílé víno Chardonnay', 3, 199),
+
+('2025-01-02', 'Růžové víno Rosé', 3, 159),
+('2025-01-05', 'Růžové víno Rosé', 4, 169),
+('2025-01-07', 'Růžové víno Rosé', 2, 179),
+
+('2025-01-01', 'Sekt Brut', 5, 229),
+('2025-01-03', 'Sekt Brut', 5, 229),
+('2025-01-06', 'Sekt Brut', 5, 229);
+
